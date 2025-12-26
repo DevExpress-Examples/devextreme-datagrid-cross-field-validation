d56c9b209ab6f28d16b913334341a854252fac36
rodgedeluna-ds <rodge.deluna@devsoft.ph>
2025-12-26 10:42:59 +0800

    Add Angular example

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, ViewChild } from '@angular/core';
+import { DxDataGridComponent, type DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import Validator from 'devextreme/ui/validator';
+import type { DxSelectBoxTypes } from 'devextreme-angular/ui/select-box';
+import type { DxDateBoxTypes } from 'devextreme-angular/ui/date-box';
+import type { ValidationCallbackData } from 'devextreme-angular/common';
+import { EmployeeModel, Service } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +12,56 @@ import { ClickEvent } from 'devextreme/ui/button';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular';
+  @ViewChild('grid', { static: false }) grid!: DxDataGridComponent;
 
-  counter = 0;
+  service: Service;
 
-  buttonText = 'Click count: 0';
+  employees: EmployeeModel[];
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  editingModes = [
+    { text: 'Row', value: 'row' },
+    { text: 'Form', value: 'form' },
+    { text: 'Popup', value: 'popup' },
+  ];
+
+  editingMode = 'row';
+
+  constructor(service: Service) {
+    this.service = service;
+    this.employees = service.getEmployees();
   }
+
+  changeEditingMode(e: DxSelectBoxTypes.ValueChangedEvent): void {
+    this.grid.instance.option('editing.mode', e.value);
+  }
+
+  onInitNewRow(e: DxDataGridTypes.InitNewRowEvent): void {
+    e.data.BirthDate = null;
+    e.data.HireDate = null;
+  }
+
+  onEditorPreparing(e: DxDataGridTypes.EditorPreparingEvent): void {
+    const mode = e.component.option('editing.mode') as string;
+    const isGridCellMode = ['batch', 'cell', 'row'].includes(mode);
+
+    if (e.dataField === 'BirthDate' && e.parentType === 'dataRow') {
+      const defaultHandler = e.editorOptions.onValueChanged;
+
+      e.editorOptions.onValueChanged = (args: DxDateBoxTypes.ValueChangedEvent): void => {
+        defaultHandler(args);
+
+        if (e.row?.rowIndex !== undefined) {
+          const hireCell = e.component.getCellElement(e.row.rowIndex, 'HireDate');
+
+          if (hireCell) {
+            const validator = isGridCellMode ? (Validator.getInstance(hireCell) as Validator) : (Validator.getInstance(hireCell.querySelector('.dx-texteditor') as Element) as Validator);
+
+            validator?.validate();
+          }
+        }
+      };
+    }
+  }
+
+  hireDateValidationCallback = (params: ValidationCallbackData): boolean => new Date(params.value) > new Date(params.data.BirthDate);
 }
---
+++ Angular/src/app/app.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+export interface EmployeeModel {
+  ID: number;
+  FirstName: string;
+  BirthDate: string | Date | null;
+  HireDate: string | Date | null;
+}
+
+const employeesData: EmployeeModel[] = [
+  {
+    ID: 1,
+    FirstName: 'John',
+    BirthDate: '1995/01/01',
+    HireDate: '1995/01/15',
+  },
+  {
+    ID: 2,
+    FirstName: 'Olivia',
+    BirthDate: '1981/06/03',
+    HireDate: '2012/05/14',
+  },
+  {
+    ID: 3,
+    FirstName: 'Robert',
+    BirthDate: '1974/09/07',
+    HireDate: '2002/11/08',
+  },
+  {
+    ID: 4,
+    FirstName: 'Greta',
+    BirthDate: '1977/11/22',
+    HireDate: '1998/04/23',
+  },
+];
+
+@Injectable()
+export class Service {
+  getEmployees(): EmployeeModel[] {
+    return employeesData;
+  }
+}
